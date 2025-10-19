e99ccbdbdf64d0f271e65c506a8346b33dbe71a5
Mourad <soltanibusiness2030@gmail.com>
2025-10-19 23:15:46 +0100

    Create config.js
    configuration needed to sync media files from 
    google drive

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js — place in same folder as Sharafia-2.html
+window.SHARQIA_CONFIG = {
+  googleDrive: {
+    folderId: "1Nliy-J4c1wssRUiZhlebanbOOF1l07yX",   // your Drive folder ID
+    apiKey: "AIzaSyDtuuNuM39rB0ofnZt7qKb30Woau8DcFpQ" // your API key
+  }
+};
